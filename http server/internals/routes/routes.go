d81aeaaab75e252bcb2653505ac6f01bc1c5cd97
dev-karani <edwin.k.kithinji@gmail.com>
2026-05-02 08:53:47 +0300

    feat: set up cli parse flags, chi router & api handlers

--- http server/internals/routes/routes.go
+++ http server/internals/routes/routes.go
@@ -1,1 +1,14 @@
 package routes
+
+import (
+	"github.com/dev-karani/http-server/internals/app"
+	"github.com/go-chi/chi/v5"
+)
+
+func SetupRoutes(app *app.Application) *chi.Mux {
+	r := chi.NewRouter()
+	r.Get("/health", app.HealthCheck)
+	r.Get("/workouts/{id}", app.WorkoutHandler.HandleGetWorkoutByID)
+	r.Post("/workouts", app.WorkoutHandler.HandleCreateWorkout)
+	return r
+}
--- http server/main.go
+++ http server/main.go
@@ -4,27 +4,33 @@ import (
 	"fmt"
 	"net/http"
 	"time"
-
+	"flag"
 	"github.com/dev-karani/http-server/internals/app"
-)
+	"github.com/dev-karani/http-server/internals/routes"
 
+)
+ 
 func main(){
+	var port int
+	flag.IntVar(&port, "port", 8080, "go backend sever port")
+	flag.Parse()
+
 	app, err := app.NewApplication() 
 
 	if err !=nil {
 		panic(err)
 	}
 
-	app.Logger.Println("we are running out app")
-
-	http.HandleFunc("/health", HealthCheck)
-	
+	r := routes.SetupRoutes(app)
 	server := &http.Server{
-		Addr: ":8080",
+		Addr: fmt.Sprintf(":%d", port),
+		Handler: r,
 		IdleTimeout: time.Minute,
 		ReadTimeout: 10 *time.Second,
 		WriteTimeout: 10*time.Second,
 	}
+
+	app.Logger.Printf("we are running on port %d\n", port)
 
 	err = server.ListenAndServe()
 	if err!= nil {
@@ -32,6 +38,3 @@ func main(){
 	}
 }
 
-func HealthCheck(w http.ResponseWriter, r *http.Request){
-	fmt.Fprintf(w, "status is available")
-}
